54c02bd1ee92ef8a8879a648c1450ee90f6e5a6b
SamShtram <159603396+SamShtram@users.noreply.github.com>
2025-10-24 17:52:36 -0400

    Create closest_pair.py

---
+++ src/closest_pair.py
@@ -0,0 +1,22 @@
+from fastdtw import fastdtw
+import numpy as np
+
+def find_closest_pair(cluster):
+    """
+    Find the most similar pair of signals in a given cluster using DTW distance.
+    Returns ((idx1, idx2), distance)
+    """
+    if len(cluster) < 2:
+        return (None, None), 0.0
+
+    min_dist = float('inf')
+    pair = (None, None)
+
+    for i in range(len(cluster)):
+        for j in range(i + 1, len(cluster)):
+            dist, _ = fastdtw(cluster[i], cluster[j])
+            if dist < min_dist:
+                min_dist = dist
+                pair = (i, j)
+
+    return pair, min_dist
